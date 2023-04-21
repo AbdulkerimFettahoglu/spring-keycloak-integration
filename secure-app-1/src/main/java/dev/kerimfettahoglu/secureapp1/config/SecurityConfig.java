bb25918e3af65aa34fc861b25ec6c3686304306e
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-04-21 10:08:50 +0300

    configuration that is related with jwt.

---
+++ secure-app-1/src/main/java/dev/kerimfettahoglu/secureapp1/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package dev.kerimfettahoglu.secureapp1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	http.authorizeRequests().antMatchers("/public/**").permitAll();
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+    }
+
+}
