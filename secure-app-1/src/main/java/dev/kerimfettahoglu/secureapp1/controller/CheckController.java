36224610413e29febfc42f123b539f2bafe3e6e0
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-04-22 17:41:21 +0300

    A web service created that is provide auths for spring security added.

--- secure-app-1/src/main/java/dev/kerimfettahoglu/secureapp1/controller/CheckController.java
+++ secure-app-1/src/main/java/dev/kerimfettahoglu/secureapp1/controller/CheckController.java
@@ -1,8 +1,15 @@
 package dev.kerimfettahoglu.secureapp1.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/check")
@@ -10,5 +17,16 @@ public class CheckController {
     @GetMapping("/up")
     public String check() {
         return "Up";
+    }
+
+    @GetMapping(path = "/auths")
+    public Map<String,Object> auths(JwtAuthenticationToken principal) {
+        Collection<String> authorities = principal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        Map<String,Object> info = new HashMap<>();
+        info.put("name", principal.getName());
+        info.put("authorities", authorities);
+        return info;
     }
 }
