c543bc0d046d22a5f6d2f8ebf87051ef059cf72f
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-04-23 01:04:11 +0300

    restTemplate bean modified to use JWT interceptor.

--- secure-app-1/src/main/java/dev/kerimfettahoglu/secureapp1/SecureApp1Application.java
+++ secure-app-1/src/main/java/dev/kerimfettahoglu/secureapp1/SecureApp1Application.java
@@ -1,13 +1,33 @@
 package dev.kerimfettahoglu.secureapp1;
 
+import dev.kerimfettahoglu.secureapp1.config.JwtTokenInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SecureApp1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SecureApp1Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        if (CollectionUtils.isEmpty(interceptors)) {
+            interceptors = new ArrayList<>();
+        }
+        interceptors.add(new JwtTokenInterceptor());
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 
 }
